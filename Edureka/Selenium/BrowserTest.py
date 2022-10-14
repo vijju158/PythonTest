c5d172897720d22890d2a87dde4379aa70916a25
vijay <vijayumar.malladi@gmail.com>
2022-10-14 09:54:58 +0530

    Selenium Version

--- Edureka/Selenium/BrowserTest.py
+++ Edureka/Selenium/BrowserTest.py
@@ -1,7 +1,9 @@
 
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(executable_path="D:\Softwares\ChromeDriver\\chromedriver.exe")
+#driver = webdriver.Chrome(executable_path="D:\Softwares\ChromeDriver\\chromedriver.exe")
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 driver.get("https://demo.guru99.com/v4/")
 driver.maximize_window()
 title=driver.title
